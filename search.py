51502206ef316f344ecb2427b7ad831cb08249e7
arrowboy47 <aidenquinkelly@gmail.com>
2023-02-26 00:00:19 -0800

    regex start

--- search.py
+++ search.py
@@ -1,18 +1,11 @@
 import re
 def sexbot(comment_body):
     score = 0
-    good = re.findall(comment_body, B)
-    bad = re.findall(comment_body, B)
+    good_patt = re.compile(r'gl|good|W|fire')
+    bad_patt = re.compile(r'rl|bad|off|return')
+    good = re.findall(comment_body, good_patt)
+    bad = re.findall(comment_body, bad_patt)
     good_score = len(good)
     bad_score = len(bad)
-    
-    return(score)
-
-
-
-
-
-
-
-
-
+    score = good_score + bad_score
+    return(score) 
