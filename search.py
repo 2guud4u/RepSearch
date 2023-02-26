1401d59d5c77da03835063f7161d29c1628fa850
arrowboy47 <aidenquinkelly@gmail.com>
2023-02-25 23:37:53 -0800

    regex started

---
+++ search.py
@@ -0,0 +1,18 @@
+import re
+def sexbot(comment_body):
+    score = 0
+    good = re.findall(comment_body, B)
+    bad = re.findall(comment_body, B)
+    good_score = len(good)
+    bad_score = len(bad)
+    
+    return(score)
+
+
+
+
+
+
+
+
+
