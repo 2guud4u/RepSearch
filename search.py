8332a53e91856f95d42635880b9655b3c7188209
arrowboy47 <aidenquinkelly@gmail.com>
2023-02-27 21:52:22 -0800

    regex improved

--- search.py
+++ search.py
@@ -1,11 +1,11 @@
 import re
-def sexbot(comment_body):
+def sexbot(comment_body): 
     score = 0
-    good_patt = re.compile(r'gl|good|W|fire')
-    bad_patt = re.compile(r'rl|bad|off|return')
+    good_patt = re.compile(r'(gl)|(good)|(W\.*\s+)|(fire)', re.IGNORECASE)
+    bad_patt = re.compile(r'(rl)|(bad)|(off)|(return)', re.IGNORECASE)
     good = re.findall(comment_body, good_patt)
     bad = re.findall(comment_body, bad_patt)
     good_score = len(good)
     bad_score = len(bad)
     score = good_score + bad_score
-    return(score) 
+    return(score)
