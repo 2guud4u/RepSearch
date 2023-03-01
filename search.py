a796f634fa901719195c845c3917d7f9ad0bda53
arrowboy47 <aidenquinkelly@gmail.com>
2023-02-28 22:58:22 -0800

    sentiment analysis

--- search.py
+++ search.py
@@ -1,5 +1,6 @@
 import re
-def sexbot(comment_body): 
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+def keywords(comment_body): 
     score = 0
     good_patt = re.compile(r'gl|good|\.*\sW\.*\s+|fire', re.IGNORECASE)
     bad_patt = re.compile(r'rl|\sbad|\soff|return', re.IGNORECASE)
@@ -10,4 +11,23 @@ def sexbot(comment_body):
     score = good_score + bad_score
     return(score)
 
+def sentiment_scores(comment_body):
 
+    # creates SentimentIntensityAnalyzer object
+    sid_obj = SentimentIntensityAnalyzer()
+    scores = sid_obj.polarity_scores(comment_body)
+    if scores['compound'] >= 0.05:
+        return("Positive")
+ 
+    elif scores['compound'] <= - 0.05:
+        return("Negative")
+ 
+    else:
+        return("Neutral")
+
+
+
+c = "man these shoes look bad u can see the stitches RL"
+
+d = sentiment_scores(c)
+print(d)
