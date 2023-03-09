f0c806735bcb0563dfef9bef991b1f1b55f2121e
Jiahua Jiang <jiahua212@gmail.com>
2023-03-08 16:12:21 -0800

    search bar working

--- projectEnv/app.py
+++ projectEnv/app.py
@@ -3,18 +3,17 @@ from searchCode import searchItem
 from flask import Flask
 app = Flask(__name__)
 
-@app.route("/", methods=['GET', 'POST'])#search front page 
-def home():
-    return "Hello, Flask!"
 
-@app.route("/results/<name>", methods=['GET', 'POST'])#search results
-def results(name):
-    result=str(searchItem(name)[0])
+@app.route("/results", methods=['GET', 'POST'])#search results
+def results():
+    if request.method == "POST":
+        searchVal = request.form.get("sVal")
+        result=str(searchItem(searchVal)[-1])
    
     return render_template("results.html", result=result)
 
-@app.route("/index/<input>")
-def index(input):
+@app.route("/")
+def index():
 
-    return render_template("index.html", input = input)
+    return render_template("index.html")
 
