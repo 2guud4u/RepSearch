be1ab47900076635d784b0e57764f13cbae7edd6
Jiahua Jiang <jiahua212@gmail.com>
2023-05-07 15:37:59 -0700

    added search caching

--- projectEnv/app.py
+++ projectEnv/app.py
@@ -24,13 +24,15 @@ db = SQLAlchemy(app)
 @app.route("/results", methods=['GET', 'POST'])#search results
 def results():
     #down here to avoid circular imports
-    from dataModels import Product
+    from databases.dataModels import Product
+    
     with app.app_context():
         db.create_all()
+
     if request.method == "POST":
         searchVal = request.form.get("sVal")
         
-        result=searchItem(searchVal)
+        result=searchItem(db, searchVal)
         #if no results
         if len(result) == 0:
             return render_template("empty.html", searchVal=searchVal)
@@ -38,14 +40,14 @@ def results():
     #store info into database
         date = datetime.now().time()
         for item in result:
-            
-            data = Product(post_id=item.post_id,
-                            score=item.rating, 
-                            date_updated=date,
-                            title=item.title, 
-                            url=item.url)
-            db.session.add(data)  # Add the instance to the session
-            db.session.commit()  # Commit the session to persist the data in the database
+            if item.cached == False:
+                data = Product(post_id=item.post_id,
+                                score=item.rating, 
+                                date_updated=date,
+                                title=item.title, 
+                                url=item.url)
+                db.session.merge(data)  # Add the instance to the session
+                db.session.commit()  # Commit the session to persist the data in the database
 
     return render_template("results.html", result=result)
 
--- projectEnv/searchCode.py
+++ projectEnv/searchCode.py
@@ -1,6 +1,8 @@
 import praw
 from praw.models import MoreComments
 from search import *
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 
 #TODO change login into oauth instead of password
@@ -11,16 +13,25 @@ r = praw.Reddit(
 )
 #post object
 class post:
-    def __init__(self, url, rating, title, wtc, post_id):
+    def __init__(self, url, rating, title, wtc, post_id, cached):
         self.url = url
         self.rating = rating
         self.title = title
         self.wtc = wtc 
         self.post_id = post_id
+        self.cached = cached
     def __str__(self):
         return f'Post name is {self.title} with rating {self.rating} Link: {self.url} id: {self.url}\n'
     
-
+def get_data_if_exist(p_key):
+    from databases.dataModels import Product
+    record = Product.query.get(p_key)
+    
+    if record is not None:
+        return record.score
+    else:
+        return False
+    
 def sortRating(s):
     return s.rating
 
@@ -30,7 +41,7 @@ def getRating(comments):
     for comment in comments:
         rating += sentiment_scores(comment.body)
     return rating
-def searchItem(prompt):
+def searchItem(db,prompt):
     post_list = []
 
     #grabs posts based on prompt
@@ -38,10 +49,18 @@ def searchItem(prompt):
                                                     sort="relevance", 
                                                     limit=5, 
                                                     time_filter= "year"):
+        #look for cached data
+        cached= True
+        rating = get_data_if_exist(s.id)    
+        if rating == False:
+            rating = getRating(s.comments)
+            cached = False
         post_list.append(post("https://www.reddit.com"+ s.permalink, 
-                            getRating(s.comments), 
+                            rating, 
                             s.title, 
-                            s.url, s.id))
+                            s.url, 
+                            s.id, 
+                            cached))
 
     post_list.sort(key=sortRating, reverse = True)#sort the post obj in order of rating
     
