bbf355297cfd511d48abc9503240ad5682361871
Jiahua Jiang <jiahua212@gmail.com>
2023-09-10 21:54:36 -0700

    able to return json now

---
+++ dev/FlaskServer/searchCode.py
@@ -0,0 +1,122 @@
+import praw
+from praw.models import MoreComments
+from search import *
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+from praw.models.util import stream_generator
+
+cur_prompt= ""
+
+post_list = []
+total_post_list = []
+#TODO change login into oauth instead of password
+r = praw.Reddit(
+    client_id='jknOULmDh_Xkmi5xLSpl_A',
+    client_secret='5jOGVzfdgGJgRrxS7oPZAzaBZnndEA',
+    user_agent="smol man",
+)
+#post object
+class post:
+    def __init__(self, url, rating, title, wtc, post_id, cached):
+        self.url = url
+        self.rating = rating
+        self.title = title
+        self.wtc = wtc 
+        self.post_id = post_id
+        self.cached = cached
+    def __str__(self):
+        return f'Post name is {self.title} with rating {self.rating} Link: {self.url} id: {self.url}\n'
+    def id(self):
+        print(self.post_id)
+    def serialize(self):
+        return {"url": self.url,
+                "rating": self.rating,
+                "title": self.title,
+                "wtc": self.wtc,
+                "post_id": self.post_id,
+                "cached": self.cached}
+
+def get_data_if_exist(p_key):
+    from databases.dataModels import Product
+    record = Product.query.get(p_key)
+    
+    if record is not None:
+        print("found", p_key)
+        return (True,record.score)
+    else:
+        print("not found", p_key)
+        return (False, 0)
+    
+def sortRating(s):
+    return s.rating
+
+def getRating(prpost):   
+    rating = 0
+    sentrating = 0
+    sentnum = 0
+    prpost.comments.replace_more(limit=None) #replace all unloaded comment obj with loaded comments
+    # rating from the comment lin reg
+    baserate = linreg(prpost)
+    for comment in prpost.comments:
+        sentrating += sentiment_scores(comment.body)
+        sentnum = sentnum + 1
+    rating = baserate * (1 + (sentrating / sentnum))
+    return rating
+
+
+
+def addToPosts(p_data):
+#look for cached data
+    
+    # rating = get_data_if_exist(p_data.id) 
+    # rating = [False]
+    # if rating[0] == False:
+        
+    print("getting ratings", p_data.id)
+    rating = (False,getRating(p_data))
+    
+    print("rating obtained")
+#add to processed post
+    print("not cached")
+    #add to processed post
+    
+    post_list.append(post("https://www.reddit.com"+ p_data.permalink, 
+                        rating[1], 
+                        p_data.title, 
+                        p_data.url, 
+                        p_data.id, 
+                        rating[0]))
+
+    
+def searchItem(db,prompt):
+
+    # purge old processed_ids and posts
+    
+    post_list.clear()
+    total_post_list.clear()
+    #grabs posts based on prompt
+    for i in r.subreddit("FashionReps").search(query=prompt,
+                                                    sort="relevance", 
+                                                    limit=50, 
+                                                    time_filter= "year"):
+        total_post_list.append(i)
+    #add first 6 posts
+    print("looked for posts", len(total_post_list))
+    for s in total_post_list[:6]:
+        addToPosts(s)
+        
+    post_list.sort(key=sortRating, reverse = True)#sort the post obj in order of rating
+   
+    return post_list
+
+def loadMore():
+    #add 6 more
+    #remove previous 6
+    del total_post_list[:6]
+    for s in total_post_list[:6]:
+        print("loaded one")
+        addToPosts(s)
+
+    
+    post_list.sort(key=sortRating, reverse = True)#sort the post obj in order of rating
+    return post_list
--- dev/FlaskServer/server.py
+++ dev/FlaskServer/server.py
@@ -1,14 +1,71 @@
 from flask import *
-import random
+from searchCode import searchItem, loadMore
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import logging
+from datetime import datetime
+import glob 
+import time
 from flask_cors import CORS
+import json
+
+# logging.basicConfig()
+# logging.getLogger('sqlalchemy.engine').setLevel(logging.INFO)
+
+#from dataModels import db, Product
 
 app = Flask(__name__)
 CORS(app)
-# Path for our main Svelte page
-@app.route("/rand")
-def hello():
-    return str(random.randint(0, 100)) + " is a random number"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///./posts.db"
 
+
+db = SQLAlchemy(app)
+cur_form = {}
+app.secret_key = 'your-secret-key'
+
+def serialize(self):
+        return {"url": self.url,
+                "rating": self.rating,
+                "title": self.title,
+                "wtc": self.wtc,
+                "post_id": self.post_id,
+                "cached": self.cached}
+
+ 
+@app.route("/results", methods=['GET', 'POST'])#search results
+def results():
+    #down here to avoid circular imports
+    from databases.dataModels import Product
+    
+    with app.app_context():
+        db.create_all()
+    info = session.get('form_data')
+    if "fromIndex" in info:
+        
+            searchVal = info.get("sVal")
+            
+            result=searchItem(db, searchVal)
+            #if no results
+            if len(result) == 0:
+                return render_template("empty.html", searchVal=searchVal)
+    if "fromResults" in request.form:
+        result = loadMore()
+    
+            
+           
+    for r in result:
+         print(r)
+    return jsonify(result[0].serialize())
+
+
+
+
+@app.route("/loading", methods=['GET', 'POST'])
+def loading():
+    gifList = glob.glob("static/gifs/*.gif")
+    session['form_data'] = request.form
+    
+    return render_template("loading.html", gifList=gifList)
 
 if __name__ == "__main__":
     app.run(debug=True)
